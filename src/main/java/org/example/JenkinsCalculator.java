53891619fd8159e0fd02eb9a44f113b3c45afd82
Vasco Costa <vascocosta.89@gmail.com>
2023-03-21 15:50:16 +0000

    update

--- src/main/java/org/example/JenkinsCalculator.java
+++ src/main/java/org/example/JenkinsCalculator.java
@@ -3,8 +3,7 @@ package org.example;
 public class JenkinsCalculator {
 
     public int addNumbers(int numberOne, int numberTwo){
-        int result = numberOne+numberTwo;
-        return result;
+        return numberOne+numberTwo;
     }
 
     public int subtractNumbers(int numberOne, int numberTwo){
